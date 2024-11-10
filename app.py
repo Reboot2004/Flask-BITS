482372be45eece104a27076b49deea8400356bd0
Harshith <knssriharshith@gmail.com>
2024-11-10 11:33:33 +0530

    v

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ def predict():
         return jsonify({'error': 'Processing error occurred.'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=8000)
